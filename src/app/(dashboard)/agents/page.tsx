0c58527be3ef9b9a61a777e242893878390d49b1
Bin Hu <elithrade@gmail.com>
2025-07-20 13:17:42 +0800

    scaffold agents procedure and ui

---
+++ src/app/(dashboard)/agents/page.tsx
@@ -0,0 +1,11 @@
+import { AgentsView } from "@/modules/agents/ui/views/agents-view";
+
+const Page = () => {
+  return (
+    <div>
+      <AgentsView />
+    </div>
+  );
+};
+
+export default Page;
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,16 +1,5 @@
 "use client";
 
-import { authClient } from "@/lib/auth-client";
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const { data: session } = authClient.useSession();
-  const trpc = useTRPC();
-
-  const { data } = useQuery(
-    trpc.hello.queryOptions({ text: session?.user?.name || "Guest" }),
-  );
-
-  return <div className="p-4 flex flex-col gap-y-4">{data?.greeting}</div>;
+  return <div className="p-4 flex flex-col gap-y-4">Home view</div>;
 };
