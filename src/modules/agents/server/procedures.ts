e9af8c274fc985aac644c5cd159bd46369e2eff2
Bin Hu <elithrade@gmail.com>
2025-07-23 09:00:11 +0800

    add procedure to get one agent by id

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
+import { eq } from "drizzle-orm";
 import {
   createTRPCRouter,
   baseProcedure,
@@ -7,13 +8,23 @@ import {
 } from "@/trpc/init";
 // import { TRPCError } from "@trpc/server";
 import { agentInsertSchema } from "../schemas";
+import z from "zod";
 
 export const agentsRouter = createTRPCRouter({
+  // TODO: Change getOne to use protectedProcedure
+  getOne: baseProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+
+      return existingAgent;
+    }),
   // TODO: Change getMany to use protectedProcedure
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
-    // await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate a delay
-    // throw new TRPCError({ code: "BAD_REQUEST" });
     return data;
   }),
   create: protectedProcedure
