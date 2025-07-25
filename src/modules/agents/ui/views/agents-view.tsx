dba24b17b02b0e8b26b5547d2ae0898481e0e784
Bin Hu <elithrade@gmail.com>
2025-07-25 20:03:55 +0800

    add data-table to render mock data

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,10 +2,25 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns, Payment } from "../components/columns";
+
+const mockData = [
+  {
+    id: "729ed5sf",
+    amount: 100,
+    status: "pending",
+    email: "email@example.com",
+  },
+] satisfies Payment[];
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      <DataTable data={mockData} columns={columns} />
+    </div>
+  );
 };
