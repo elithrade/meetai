94b02652f98941d7d3cf48d3a86272dfe038e895
Bin Hu <elithrade@gmail.com>
2025-07-28 21:06:26 +0800

    add page pagination

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -6,10 +6,11 @@ import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
-  const [filters] = useAgentsFilters();
+  const [filters, setFilters] = useAgentsFilters();
   const { data } = useSuspenseQuery(
     trpc.agents.getMany.queryOptions({ ...filters }),
   );
@@ -17,7 +18,14 @@ export const AgentsView = () => {
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
       {data.items.length > 0 ? (
-        <DataTable data={data.items} columns={columns} />
+        <>
+          <DataTable data={data.items} columns={columns} />
+          <DataPagination
+            page={filters.page}
+            totalPages={data.totalPages}
+            onPageChange={(page) => setFilters({ page })}
+          />
+        </>
       ) : (
         <EmptyState
           title="Create your first agent"
