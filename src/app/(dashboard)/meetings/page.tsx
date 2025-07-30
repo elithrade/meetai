5d046dd7a83c5802a55674f4cb8f053bf7ca223d
Bin Hu <elithrade@gmail.com>
2025-07-30 22:59:49 +0800

    initial meetings view and procedures

---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,11 @@
+import { MeetingsView } from "@/modules/meetings/ui/views/meetings-view";
+
+const Page = () => {
+  return (
+    <div>
+      <MeetingsView />
+    </div>
+  );
+};
+
+export default Page;
