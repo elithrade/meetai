96f003e33454dd06c4ce982a24d61d629f2dad28
Bin Hu <elithrade@gmail.com>
2025-08-03 10:21:18 +0800

    implement call view

--- src/modules/call/ui/views/call-view.tsx
+++ src/modules/call/ui/views/call-view.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { ErrorState } from "@/components/error-state";
+import { MeetingStatus } from "@/modules/meetings/types";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { CallProvider } from "../components/call-provider";
 
 type Props = {
   meetingId: string;
@@ -12,5 +15,16 @@ export const CallView = ({ meetingId }: Props) => {
     trpc.meetings.getOne.queryOptions({ id: meetingId }),
   );
 
-  return <div>Call view {JSON.stringify(data)}</div>;
+  if (data.status === MeetingStatus.Completed) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <ErrorState
+          title="Meeting ended"
+          description="This meeting has already ended."
+        />
+      </div>
+    );
+  }
+
+  return <CallProvider meetingId={meetingId} meetingName={data.name} />;
 };
