7948f78e2f5f0067ade128b6cfdc6e64eda09e1e
Bin Hu <elithrade@gmail.com>
2025-08-11 19:49:43 +0800

    remove cancel meeting button

--- src/modules/meetings/ui/components/upcoming-state.tsx
+++ src/modules/meetings/ui/components/upcoming-state.tsx
@@ -1,6 +1,6 @@
 import { EmptyState } from "@/components/empty-state";
 import { Button } from "@/components/ui/button";
-import { BanIcon, VideoIcon } from "lucide-react";
+import { VideoIcon } from "lucide-react";
 import Link from "next/link";
 
 type Props = {
@@ -8,11 +8,7 @@ type Props = {
   onCancelMeeting: () => void;
   isCancelling: boolean;
 };
-export const UpcomingState = ({
-  meetingId,
-  onCancelMeeting,
-  isCancelling,
-}: Props) => {
+export const UpcomingState = ({ meetingId, isCancelling }: Props) => {
   return (
     <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
       <EmptyState
@@ -21,15 +17,6 @@ export const UpcomingState = ({
         description="This meeting is not started. Once the meeting starts, meeting details will be available here."
       />
       <div className="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-2 w-full">
-        <Button
-          variant="secondary"
-          className="w-full lg:w-auto"
-          onClick={onCancelMeeting}
-          disabled={isCancelling}
-        >
-          <BanIcon />
-          Cancel meeting
-        </Button>
         <Button disabled={isCancelling} asChild className="w-full lg:w-auto">
           <Link href={`/call/${meetingId}`}>
             <VideoIcon />
